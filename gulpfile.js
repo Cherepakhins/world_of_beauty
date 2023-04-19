a83091d771694937ab358fa755c995790dae19da
Cherepakhins <antoncerrek@gmail.com>
2023-04-19 20:57:14 +0700

    fixed bags

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,71 @@
-const { src, dest, series, parallel, watch } = require("gulp");
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require("gulp-sass")(require("sass"));
-const clean = require("gulp-clean");
-const browserSync = require("browser-sync").create();
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-const sourcemaps = require("gulp-sourcemaps");
-const webpackStream = require("webpack-stream");
-const rename = require("gulp-rename");
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+const webpackStream = require('webpack-stream');
+const rename = require('gulp-rename');
 
 // Таск компиляции SASS в CSS
 function buildSass() {
-  return src("src/scss/**/*.scss")
+  return src('src/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        includePaths: ["./node_modules"],
-      }).on("error", sass.logError)
-    )
+    .pipe(sass())
+    .on('error', sass.logError)
     .pipe(
       postcss([
         autoprefixer({
           grid: true,
-          overrideBrowserslist: ["last 2 versions"],
+          overrideBrowserslist: ['last 2 versions'],
         }),
         cssnano(),
       ])
     )
-    .pipe(sourcemaps.write())
-    .pipe(dest("src/css"))
-    .pipe(dest("dist/css"))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('src/css'))
+    .pipe(dest('dist/css'))
     .pipe(browserSync.stream());
 }
+
 function buildJs() {
-  return src("src/js/index.js")
-    .pipe(webpackStream(require("./webpack.config")))
-    .pipe(rename("main.min.js"))
-    .pipe(dest("src/js"))
-    .pipe(dest("dist/js"))
+  return src('src/js/index.js')
+    .pipe(webpackStream(require('./webpack.config')))
+    .pipe(rename('main.min.js'))
+    .pipe(dest('src/js'))
+    .pipe(dest('dist/js'))
     .pipe(browserSync.stream());
 }
+
 // Таск работы с html файлами
-function buildHtml() {
-  return src("src/**/*.html").pipe(dest("dist")).pipe(browserSync.stream());
+function html() {
+  return src('src/**/*.html').pipe(dest('dist/')).pipe(browserSync.stream())
 }
+
 function browsersync() {
   browserSync.init({
-    server: "src/",
+    server: 'src/',
     notify: false,
   });
 }
+
 // Таск отслеживания изменения файлов и запуск сервера
 function serve() {
-  watch(["src/js/**/*.js", "!src/js/**/*.min.js"], buildJs);
-  watch("src/scss/**/*.scss", buildSass);
-  watch("src/**/*.html", html);
+  watch(['src/js/**/*.js', '!src/js/**/*.min.js'], buildJs);
+  watch('src/scss/**/*.scss', buildSass);
+  watch('src/**/*.html', html);
 }
-// Таск копирования статичных файлов
+
 function copy() {
-  return src(["src/img/**/*.*"]).pipe(dest("dist/img"));
+  return src(['src/img/**/*.*', 'src/css/**/*.css'], {
+    base: 'src/',
+  }).pipe(dest('dist'));
 }
 
-// Таск очистки dist
 function cleanDist() {
-  return src("dist", { allowEmpty: true }).pipe(clean());
-}
-
-// Таск отслеживания изменения файлов
-function serve() {
-  watch("src/scss/**/*.scss", buildSass);
-  watch("src/**/*.html", buildHtml);
-}
-
-// Создание дев-сервера
-function createDevServer() {
-  browserSync.init({
-    server: "src",
-    notify: false,
-  });
+    return src('dist', { allowEmpty: true }).pipe(clean());
 }
 
 exports.build = series(cleanDist, buildSass, buildJs, html, copy);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  devtool: isProduction ? "none" : "inline-source-map",
   module: {
     rules: [
       {
